3dd81b6a37f89fc691863e50f2e6eed043f7c5e6
Majesty 17 <davanoraindikaprivate@gmail.com>
2022-03-14 18:24:36 +0800

    web3

---
+++ odyssey-frontend/header/MenuDesktop.tsx
@@ -0,0 +1,61 @@
+   
+import {Container, Icon, Stack, SvgIcon, Typography} from "@mui/material";
+import {NextRouter, useRouter} from "next/router";
+import {Link as ScrollLink} from "react-scroll";
+
+export const odysseyMenuData = [
+    {
+        href: "/",
+        tabContent: "Mint"
+    },
+    {
+        href: "/about",
+        tabContent: "About"
+    },
+    {
+        href: "/roadmap",
+        tabContent: "Roadmap"
+    },
+    {
+        href: "/faq",
+        tabContent: "FAQ"
+    },
+    {
+        href: "/team",
+        tabContent: "Team"
+    }
+]
+
+function MenuDesktop() {
+
+    return (
+        <Container>
+
+            <Stack alignItems="center" justifyItems="center">
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    spacing={4}
+                    justifyItems="center"
+                >
+                    {
+                        odysseyMenuData.map((menu, index) => (
+                            <Typography variant="h4" color={'white'} component={ScrollLink} to={menu.tabContent} spy
+                                        smooth offset={-100} sx={{
+                                cursor: "pointer",
+                                textTransform: "uppercase",
+                            }} key={index}>
+                                {menu.tabContent}
+                            </Typography>
+                        ))
+                    }
+                </Stack>
+            </Stack>
+
+
+        </Container>
+    )
+
+}
+
+export default MenuDesktop
